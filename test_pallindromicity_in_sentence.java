b0cff9c8747110a0d16ae0b27fa78ddb92428e7b
Suman More <67056908+SumanMore@users.noreply.github.com>
2021-06-26 17:27:34 +0530

    Create test_pallindromicity_in_sentence.java

---
+++ test_pallindromicity_in_sentence.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println(isPalindrome("A man, a plan, a canal, Panama"));
+  }
+  public static boolean isPalindrome(String s) {
+    int i = 0, j = s.length() - 1;
+    while (i < j) {
+      while (!Character.isLetterOrDigit(s.charAt(i)) && i < j) {
+        ++i;
+      }
+      while (!Character.isLetterOrDigit(s.charAt(j)) && i < j) {
+        --j;
+      }
+      if (Character.toLowerCase(s.charAt(i++)) !=
+        Character.toLowerCase(s.charAt(j--))) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
