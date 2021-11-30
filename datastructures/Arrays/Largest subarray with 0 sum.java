69d1b8411a5323e8b50fca5654158130ee0e87f0
Suman More <67056908+SumanMore@users.noreply.github.com>
2021-11-30 22:04:23 +0530

    Update Largest subarray with 0 sum.java

--- datastructures/Arrays/Largest subarray with 0 sum.java
+++ datastructures/Arrays/Largest subarray with 0 sum.java
@@ -25,3 +25,23 @@ class GfG
       return res;
     }
 }
+
+
+
+
+class GfG {
+    int maxLen(int arr[], int n) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int res = 0, sum = 0;
+        
+        for(int i = 0; i < n; i++) {
+            sum += arr[i];
+            if(map.containsKey(sum))
+                res = Math.max(res, i - map.get(sum));
+            else
+                map.put(sum, i);
+        }
+        return res;
+    }
+}
