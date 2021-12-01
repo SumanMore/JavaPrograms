dac4874402fd521f063dc97f7ac6ed60105acec6
Suman More <67056908+SumanMore@users.noreply.github.com>
2021-12-01 20:58:00 +0530

    Create Missing_num.java

---
+++ datastructures/Arrays/Missing_num.java
@@ -0,0 +1,10 @@
+static int getMissingNo(int a[], int n)
+{
+    int n_elements_sum = n * (n + 1) / 2;
+    int sum = 0;
+ 
+    for(int i = 0; i < n - 1; i++)
+        sum += a[i];
+         
+    return n_elements_sum - sum;
+}
