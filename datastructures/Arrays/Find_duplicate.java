4290fd6749f5a8bd7c2a699b3cfa48d37787d756
Suman More <67056908+SumanMore@users.noreply.github.com>
2021-12-01 20:56:40 +0530

    Create Find_duplicate.java

---
+++ datastructures/Arrays/Find_duplicate.java
@@ -0,0 +1,6 @@
+for (i = 0; i < size; i++) {
+            int j = Math.abs(arr[i]);
+            if (arr[j] >= 0)
+                arr[j] = -arr[j];
+            else
+                System.out.print(j + " ");
