09aa1294b771f09be6108d0f9b58623697102b3c
Suman More <67056908+SumanMore@users.noreply.github.com>
2021-12-05 18:04:33 +0530

    Create String_to_integer and viceversa.java

---
+++ datastructures/Striver_SDE_SHEET/String_to_integer and viceversa.java
@@ -0,0 +1,36 @@
+public static int stringToInt(String s)
+{
+int r=0;
+int i=s.charAt(0)=='-'? 1:0;
+for(int j=i;j<s.length();j++)
+{
+int d=s.charAt(j)-'0';
+r= r*10+d;
+}
+if(i==1)
+  return -r;
+else
+  return r;  
+}
+public static String intToString(int n)
+{
+  /*
+  int c=-210;
+  String s=""+c;
+  System.out.println(s);*/
+  int num=Math.abs(n);
+  StringBuilder s=new StringBuilder();
+while (num!=0)
+{
+  int r=num%10;
+  s.append(r);
+  num/=10;
+}
+if(n<0)
+{
+  s.append('-');
+}
+return s.reverse().toString();
+}
+
+}
